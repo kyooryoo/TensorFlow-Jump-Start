df52d2af026693104782e96d83d283910008bcd9
Ling Jiang <kyooryoo@hotmail.com>
2017-12-29 14:47:46 -0500

    wotj out of mem error

--- 3_NeuralNetwork.py
+++ 3_NeuralNetwork.py
@@ -1,3 +1,10 @@
+# run the following code to enter tensorflow virtual envrionment
+# source ~/tensorflow/bin/activate
+
+# maybe due to limited memory this program always crashed with error:
+# terminate called after throwing an instance of 'std::bad_alloc'
+# what():  std::bad_alloc
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -12,12 +19,29 @@ import tensorflow as tf
 
 FLAGS = None
 
+# generate a weight variable with a small amount of noise 
+# for symmentry breaking and to prevent 0 gradients
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+# prepare a slightly positive initial bias to avoid dead neurons
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+# use function to generate convolution layer with stride 1 and padding 0
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+# use function to generate a max pooling over 2x2 blocks
+# which can downsamples a feature map by 2X
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                        strides=[1, 2, 2, 1], padding='SAME')
 
 def deepnn(x):
   """deepnn builds the graph for a deep net for classifying digits.
-  Args:
-    x: an input tensor with the dimensions (N_examples, 784), where 784 is the
-    number of pixels in a standard MNIST image.
   Returns:
     A tuple (y, keep_prob). y is a tensor of shape (N_examples, 10), with values
     equal to the logits of classifying the digit into one of 10 classes (the
@@ -25,42 +49,51 @@ def deepnn(x):
     dropout.
   """
   # Reshape to use within a convolutional neural net.
-  # Last dimension is for "features" - there is only one here, since images are
-  # grayscale -- it would be 3 for an RGB image, 4 for RGBA, etc.
+  # x is the input tensor with dimensions (N, 784), where 784 is the number
+  # of pixels in a standard MNIST image.
+  # reshape input x into a 4d tensor, with 28x28 image size and 1 feature
+  # for the grayscale channel, it would be 3 or 4 for RGB or RGBA image.
   with tf.name_scope('reshape'):
     x_image = tf.reshape(x, [-1, 28, 28, 1])
 
   # First convolutional layer - maps one grayscale image to 32 feature maps.
   with tf.name_scope('conv1'):
+    # compute 32 features for each 5x5 patch with 1 input grayscale channel
+    # why 32 features?
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
-  # Pooling layer - downsamples by 2X.
+  # Pooling layer - downsamples by 2X - to 14x14 image size.
   with tf.name_scope('pool1'):
     h_pool1 = max_pool_2x2(h_conv1)
 
   # Second convolutional layer -- maps 32 feature maps to 64.
+  # why 64 features?
   with tf.name_scope('conv2'):
     W_conv2 = weight_variable([5, 5, 32, 64])
     b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
-  # Second pooling layer.
+  # Second pooling layer - downsamples furthre to 7x7 image size.
   with tf.name_scope('pool2'):
     h_pool2 = max_pool_2x2(h_conv2)
 
   # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
   # is down to 7x7x64 feature maps -- maps this to 1024 features.
+  # why 1024 features?
   with tf.name_scope('fc1'):
     W_fc1 = weight_variable([7 * 7 * 64, 1024])
     b_fc1 = bias_variable([1024])
-
+    
+    # reshape the tensor from pooling layer into a batch of vectors
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    # final operation of multiply by weight matrix, add bias, and apply ReLU
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-  # Dropout - controls the complexity of the model, prevents co-adaptation of
-  # features.
+  # controls the complexity of the model and reuce overfitting.
+  # keep the predicted probs before dropout for turn the dropout on during
+  # training and turn the dropout off during testing.
   with tf.name_scope('dropout'):
     keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
@@ -73,38 +106,10 @@ def deepnn(x):
     y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
   return y_conv, keep_prob
 
-
-def conv2d(x, W):
-  """conv2d returns a 2d convolution layer with full stride."""
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-  """max_pool_2x2 downsamples a feature map by 2X."""
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
-
-
-def weight_variable(shape):
-  """weight_variable generates a weight variable of a given shape."""
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
-
-
-def bias_variable(shape):
-  """bias_variable generates a bias variable of a given shape."""
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
-
-
 def main(_):
-  # Import data
+  # Import data, create model x and define loss and optimizer y_
   mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-
-  # Create the model
   x = tf.placeholder(tf.float32, [None, 784])
-
-  # Define loss and optimizer
   y_ = tf.placeholder(tf.float32, [None, 10])
 
   # Build the graph for the deep net
@@ -130,8 +135,10 @@ def main(_):
 
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+    # training 20,000 iterations and may take half an hour to complete
     for i in range(20000):
       batch = mnist.train.next_batch(50)
+      # logging every 100th iteration in the training process
       if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
             x: batch[0], y_: batch[1], keep_prob: 1.0})
